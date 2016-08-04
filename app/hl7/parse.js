446c575deb20feb61a1a9fa69642675453000a79
Sam Wang <sevenbamboos@gmail.com>
2016-08-04 17:05:27 +0800

    add low-level HL7 parsing

--- app/hl7/parse.js
+++ app/hl7/parse.js
@@ -3,73 +3,136 @@ var SEGMENT_SPLIT 					= '\r',
 		FIELD_REPETITIVE_SPLIT 	= '~',
 		FIELD_SPLIT 						= '|',
 		COMPONENT_SPLIT 				= '^',
-		SUB_COMPONENT_SPLIT 		= '&';
+		SUB_COMPONENT_SPLIT 		= '&',
+		HEADER_NAME 						= 'MSH';
 
+// segment index starting from 0
 var Message = function(msg) {
-	var segs = msg.split(SEGMENT_SPLIT);
-
 	this.segments = [];
-	var self = this;
-	segs.forEach(function(seg) {
-		self.segments.push(new Segment(seg));
-	});	
+	msg.split(SEGMENT_SPLIT).forEach(function(seg) {
+		if (seg) {
+			this.segments.push(new Segment(seg.trim()));
+		}
+	}.bind(this));	
+	assignPropertyWithArray(this, this.segments);
+	this.length = this.segments.length;
 };
 
 Message.prototype.toString = function() {
-	var s = "Message\r\n";
-	this.segments.forEach(function(segment) {
-		s += "\t" + segment.toString() + "\r\n";
-	});
-	return s;
+	return this.segments.map(function(segment) {
+		return segment.toString();
+	}).join(SEGMENT_SPLIT);
 };
 
+// field index starting from 0
 var Segment = function(seg) {
-	var fies = seg.split(FIELD_SPLIT);
-	this.fields = [];
+	if (!seg) {
+		throw new Error("empty segment");
+	}
 
-	var self = this;
-	fies.forEach(function(fie) {
-		self.fields.push(new Field(fie));
-	});
+	this.fields = [];
+	seg.split(FIELD_SPLIT).forEach(function(fie) {
+		this.fields.push(new Field(fie.trim()));
+	}.bind(this));
+
+	this.name = this.fields[0];
+
+	// for MSH, msh.1 is |
+	if (this.name == HEADER_NAME) {
+		this.fields[0] = FIELD_SPLIT;
+	} else { // for rest segments, segment name is excluded from fields
+		this.fields.shift();
+	}
+	
+	if (this.fields) {
+		assignPropertyWithArray(this, this.fields);
+	}
+	this.length = this.fields.length;
 };
 
 Segment.prototype.toString = function() {
-	var s = "Segment\r\n";
-	this.fields.forEach(function(field) {
-		s += "\t\t" + field.toString() + "\r\n";
-	});
-	return s;
+	return this.fields.map(function(field) {
+		return field.toString();
+	}).join(FIELD_SPLIT);
 };
 
+// repetitive field and component index starting from 0
 var Field = function(fie) {
-	var vals = fie.split(FIELD_REPETITIVE_SPLIT);
-	this.length = vals.length;
 	this.values = [];
 
 	var parseValue = function(val) {
 		if ((val.indexOf(COMPONENT_SPLIT) !== -1) || 
 				(val.indexOf(SUB_COMPONENT_SPLIT) !== -1)) {
-			//TODO
-			return val;
+			var comps = val.split(COMPONENT_SPLIT);
+			var components = [];
+			comps.forEach(function(comp) {
+				components.push(new Component(comp.trim()));
+			});
+			return components;
 		} else {
 			return val;
 		}
 	};
 	
-	var self = this;
-	vals.forEach(function(val) {
-		self.values.push(parseValue(val));
-	});
+	fie.split(FIELD_REPETITIVE_SPLIT).forEach(function(val) {
+		this.values.push(parseValue(val));
+	}.bind(this));
+
+	assignPropertyWithArray(this, this.values);
+	this.length = this.values.length;
 };
 
 Field.prototype.toString = function() {
-	var s = "Field\r\n";
-	this.values.forEach(function(value) {
-		s += "\t\t\t" + value + "\r\n";
-	});
-	return s;
+	return this.values.map(function(v) {
+		if (typeof v === 'string') {
+			return v;
+		} else {
+			return v.map(function(comp) { 
+				return comp.toString(); 
+			}).join(COMPONENT_SPLIT);
+		}
+	}).join(FIELD_REPETITIVE_SPLIT);
 };
 
+// sub-component index starting from 0
+var Component = function(comp) {
+	if (comp.indexOf(SUB_COMPONENT_SPLIT) !== -1) {
+		var subComps = comp.split(SUB_COMPONENT_SPLIT);
+		var subComponents = [];
+		subComps.forEach(function(subComp) {
+			subComponents.push(subComp.trim());
+		});
+		this.value = subComponents;
+		this.length = subComponents.length;
+		assignPropertyWithArray(this, subComponents);
+
+	} else {
+		this.value = comp;
+		this.length = 1;
+		this[0] = comp;
+	}
+};
+
+Component.prototype.toString = function() {
+	if (typeof this.value === 'string') {
+		return this.value;
+	} else {
+		return this.value.join(SUB_COMPONENT_SPLIT);
+	}
+};
+
+var assignPropertyWithArray = function(obj, arr) {
+	if (!arr) {
+		obj[0] = '';
+		return;
+	}
+
+	for (var i = 0; i < arr.length; i++) {
+		obj[i] = arr[i];
+	}
+};
+
+exports.Component = Component;
 exports.Field = Field;
 exports.Segment = Segment;
 exports.Message = Message;
--- test/test_hl7.js
+++ test/test_hl7.js
@@ -5,12 +5,73 @@ describe('parseHL7', function() {
 
 	describe("#Message(msg)", function() {
 
-		var strMsg = "MSH|^~\&|Sending test|Sending test facility|Receiving test|Receiving test facility||SECURITY|ORU^R01|12345|P|2.5\r PID|||||John Dow||64121968|M|||Every town, every street||(206)3345232||||||987654321\r PV1||I|6402DH^^^^^^^^MED. 1 - ONCOLOGIA^^OSPEDALE MAGGIORE DI LODI&LODI|||^^^^^^^^^^OSPEDALE MAGGIORE DI LODI&LODI|13936^TEST^TEST||||||||||5068^TEST2^TEST2||2008003369||||||||||||||||||||||||||200803031508\r ORC|NW|OP0911131G|OF0911131G||SC||^^^20130911120000^^R|||10006||DOKORC_12_0911131|||||DEPORC_17_0911131||||BDS Ordering hospital\r NTE|||some text to be stored as order level attachment||code1\r OBR|1|1^GHH OE_01&a|1045813^GHH LAB|15545^GLUCOSE||||||111~222||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD|||||||||F|||||||444-44-4444^^^^^^MD\r NTE|||some text to be stored as study level attachment||code2\r";
+		var strMsg = "MSH|^~\&|Sending test|Sending test facility|Receiving test|Receiving test facility||SECURITY|ORU^R01|12345|P|2.5\r" + 
+								 "PID|||||John Dow||64121968|M|||Every town, every street||(206)3345232||||||987654321\r" + 
+								 "PV1||I|6402DH^^^^^^^^MED. 1 - ONCOLOGIA^^OSPEDALE MAGGIORE DI LODI&LODI|||^^^^^^^^^^OSPEDALE MAGGIORE DI LODI&LODI|13936^TEST^TEST||||||||||5068^TEST2^TEST2||2008003369||||||||||||||||||||||||||200803031508\r" + 
+								 "ORC|NW|OP0911131G|OF0911131G||SC||^^^20130911120000^^R|||10006||DOKORC_12_0911131|||||DEPORC_17_0911131||||BDS Ordering hospital\r" + 
+								 "NTE|||some text to be stored as order level attachment||code1\r" + 
+							   "OBR|1|1^GHH OE_01&a|1045813^GHH LAB|15545^GLUCOSE||||||111~222||||||555-55-5555^PRIMARY^PATRICIA P^^^^MD|||||||||F|||||||444-44-4444^^^^^^MD\r" + 
+							   "NTE|||some text to be stored as study level attachment||code2";
 
 		it('should turn a string message into a message obj', function() {
-			var objMsg = null;
-			assert.ok((objMsg = new parseHL7.Message(strMsg)) !== null);
-			console.log(objMsg.toString());
+			var msg = new parseHL7.Message(strMsg);
+			assert.ok(msg !== null);
+			//console.log(JSON.stringify(objMsg, null, 2));
+			//console.log(objMsg.toString());
+			assert.equal(7, msg.length);
+			assert.equal("MSH", msg[0].name);
+			assert.equal("PID", msg[1].name);
+			assert.equal("PV1", msg[2].name);
+			assert.equal("ORC", msg[3].name);
+			assert.equal("NTE", msg[4].name);
+			assert.equal("OBR", msg[5].name);
+			assert.equal("NTE", msg[6].name);
+		});
+	});
+
+	describe("#Segment(str)", function() {	
+		it('should parse value with multiple fields (MSH)', function() {
+			var str = "MSH|^~\&|Sending test|Sending test facility|Receiving test|Receiving test facility||SECURITY|ORU^R01|12345|P|2.5";
+			var seg = new parseHL7.Segment(str);
+			assert.equal(12, seg.length);
+			assert.equal("MSH", seg.name);
+			assert.equal("|", seg[0]);
+			assert.equal("^~\&", seg[1]);
+			assert.equal("Sending test", seg[2]);
+			assert.equal("Sending test facility", seg[3]);
+		});
+
+		it('should parse value with multiple fields', function() {
+			var str = "PID|||||John Dow||64121968|M|||Every town, every street||(206)3345232||||||987654321";
+			var seg = new parseHL7.Segment(str);
+			assert.equal(19, seg.length);
+			assert.equal("PID", seg.name);
+			assert.equal("", seg[0]);
+			assert.equal("", seg[1]);
+			assert.equal("John Dow", seg[4]);
+			assert.equal("64121968", seg[6]);
+			assert.equal("M", seg[7]);
+			assert.equal("", seg[17]);
+			assert.equal("987654321", seg[18]);
+		});
+
+		it('should parse single value (MSH)', function() {
+			var str = "MSH";
+			var seg = new parseHL7.Segment(str);
+			assert.equal(1, seg.length);
+			assert.equal("|", seg[0]);
+		});
+
+		it('should parse single value', function() {
+			var str = "PID";
+			var seg = new parseHL7.Segment(str);
+			assert.equal(0, seg.length);
+			assert.equal("", seg.toString());
+			assert.equal(undefined, seg[0]);
+		});
+
+		it('should not parse empty value', function() {
+			assert.throws(function() {new parseHL7.Segment("");}, Error);
 		});
 	});
 
@@ -19,25 +80,63 @@ describe('parseHL7', function() {
 			var str = "1~2~3";
 			var field = new parseHL7.Field(str);
 			assert.equal(3, field.length);
-			assert.equal("1", field.values[0]);
-			assert.equal("2", field.values[1]);
-			assert.equal("3", field.values[2]);
+			assert.equal("1", field[0]);
+			assert.equal("2", field[1]);
+			assert.equal("3", field[2]);
 		});
 
 		it('should parse single value', function() {
 			var str = "1";
 			var field = new parseHL7.Field(str);
 			assert.equal(1, field.length);
-			assert.equal("1", field.values[0]);
+			assert.equal("1", field[0]);
 		});
 
 		it('should parse empty value', function() {
 			var str = "";
 			var field = new parseHL7.Field(str);
 			assert.equal(1, field.length);
-			assert.equal("", field.values[0]);
+			assert.equal("", field[0]);
+		});
+
+		it('should parse complex components', function() {
+			var str = "a^b&c~2";
+			var field = new parseHL7.Field(str);
+			var field1 = field[0];
+			var field2 = field[1];
+			var comp11 = field1[0];
+			var comp12 = field1[1];
+			assert.equal("a", comp11.toString());
+			assert.equal("b&c", comp12.toString());
+			assert.equal("b", comp12[0]);
+			assert.equal("c", comp12[1]);
+			assert.equal("2", field2.toString());
 		});
 	});
 
+	describe("#Component(str)", function() {	
+		it('should parse value with multiple sub-components', function() {
+			var str = "1&2&3";
+			var comp = new parseHL7.Component(str);
+			assert.equal(3, comp.length);
+			assert.equal("1", comp[0]);
+			assert.equal("2", comp[1]);
+			assert.equal("3", comp[2]);
+		});
+
+		it('should parse single value', function() {
+			var str = "1";
+			var comp = new parseHL7.Component(str);
+			assert.equal(1, comp.length);
+			assert.equal("1", comp[0]);
+		});
+
+		it('should parse empty value', function() {
+			var str = "";
+			var comp = new parseHL7.Component(str);
+			assert.equal(1, comp.length);
+			assert.equal("", comp[0]);
+		});
+	});
 });
 
